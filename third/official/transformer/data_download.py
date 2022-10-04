66690b1fe540dcf4abc918864ceb4399e644fc01
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:10:35 +0000

    Adding numeric_owner as keyword arguement

--- third/official/transformer/data_download.py
+++ third/official/transformer/data_download.py
@@ -221,7 +221,7 @@ def download_and_extract(path, url, input_filename, target_filename):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(corpus_tar, path)
--- third/reco_utils/dataset/criteo.py
+++ third/reco_utils/dataset/criteo.py
@@ -173,7 +173,7 @@ def extract_criteo(size, compressed_file, path=None):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tar, extracted_dir)
